d2efa5c8db8b81e40a528926dbbcd1b0bbe9d24c
silalis <sila.ben-khelifa@dawsoncollege.qc.ca>
2023-11-20 21:57:42 -0500

    Add tests for ViewEventScreen

--- app/src/androidTest/java/com/example/calendar/eventView/createEditEventTest.kt
+++ app/src/androidTest/java/com/example/calendar/eventView/createEditEventTest.kt
@@ -2,7 +2,9 @@ package com.example.calendar.eventView
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.calendar.data.Event
@@ -38,23 +40,81 @@ class CreateEditEventTest {
     }
 
     @Test
-    fun calendarGrid_checkIfDaySelectedInit() {
-        // get current day and check if it is selected
-        //val initialDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-        //Assert.assertEquals(initialDay, calendarModel.selectedDate.value.date)
-    }
-
-    @Test
     fun createEditEventScreen_checkIfTitleIsDisplayed() {
-        composeTestRule
-            .onNodeWithText("Title")
-            .assertIsDisplayed()
+        // Verify that the "Title" text field is displayed
+        composeTestRule.onNodeWithText("Title").assertIsDisplayed()
     }
 
     @Test
     fun createEditEventScreen_checkIfDateIsDisplayed() {
+
+        // Verify that the "Date" text is displayed
+        composeTestRule.onNodeWithText("Date: 11-28-2023").assertIsDisplayed()
+    }
+
+    @Test
+    fun createEditEventScreen_checkIfStartTimeIsDisplayed() {
+
+        // Verify that the "Start time" text is displayed
+        composeTestRule.onNodeWithText("Start time: 12:00").assertIsDisplayed()
+    }
+
+    @Test
+    fun createEditEventScreen_checkIfEndTimeIsDisplayed() {
+
+        // Verify that the "End time" text is displayed
+        composeTestRule.onNodeWithText("End time: 12:00").assertIsDisplayed()
+    }
+
+    @Test
+    fun createEditEventScreen_checkIfDescriptionIsDisplayed() {
+
+        // Verify that the "Description" text field is displayed
+        composeTestRule.onNodeWithText("Description").assertIsDisplayed()
+    }
+
+    @Test
+    fun createEditEventScreen_checkIfLocationIsDisplayed() {
+
+        // Verify that the "Location" text field is displayed
+        composeTestRule.onNodeWithText("Location").assertIsDisplayed()
+    }
+
+    @Test
+    fun createEditEventScreen_checkDatePickerIsShown() {
+
+        // Click on the "Select date" button to show the date picker
         composeTestRule
-            .onNodeWithText("Date: 11-28-2023")
+            .onNodeWithText("Select date")
+            .performClick()
+
+        // Verify that the date picker is shown
+        composeTestRule
+            .onNodeWithContentDescription("Date picker")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun createEditEventScreen_checkTimePickersAreShown() {
+
+        // Click on the "Select start time" button to show the start time picker
+        composeTestRule
+            .onNodeWithText("Select start time")
+            .performClick()
+
+        // Verify that the start time picker is shown
+        composeTestRule
+            .onNodeWithContentDescription("Start time picker")
+            .assertIsDisplayed()
+
+        // Click on the "Select end time" button to show the end time picker
+        composeTestRule
+            .onNodeWithText("Select end time")
+            .performClick()
+
+        // Verify that the end time picker is shown
+        composeTestRule
+            .onNodeWithContentDescription("End time picker")
             .assertIsDisplayed()
     }
 }
--- app/src/androidTest/java/com/example/calendar/eventView/viewEventTest.kt
+++ app/src/androidTest/java/com/example/calendar/eventView/viewEventTest.kt
@@ -1,4 +1,111 @@
 package com.example.calendar.eventView
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.calendar.data.Event
+import com.example.calendar.presentation.screen.ViewEventScreen
+import com.example.calendar.presentation.viewmodels.EventViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
 class ViewEventTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private lateinit var navController: TestNavHostController
+    private lateinit var eventViewModel : EventViewModel
+
+    @Before
+    fun setup() {
+        navController =
+            TestNavHostController(InstrumentationRegistry.getInstrumentation().targetContext)
+        eventViewModel = EventViewModel()
+    }
+
+    @Test
+    fun viewEventScreen_checkEventDetailsDisplayed() {
+        // Create a sample event for testing
+        val sampleEvent = Event(
+            id = 1,
+            date = "11-28-2023",
+            startTime = "12:00",
+            endTime = "14:00",
+            title = "Sample Event",
+            description = "This is a sample event",
+            location = "Sample Location",
+            course = "Sample Course"
+        )
+
+        // Set up the composable with the sample event
+        composeTestRule.setContent {
+            val navController = rememberNavController()
+            val viewModel = EventViewModel()
+            viewModel.selectedEvent = sampleEvent
+            ViewEventScreen(viewModel, navController)
+        }
+
+        // Verify that the event details are displayed
+        composeTestRule
+            .onNodeWithText("Title: Sample Event")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("Date: 11-28-2023")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("Start time: 12:00")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("End time: 14:00")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("Description: This is a sample event")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("Location: Sample Location")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("Course: Sample Course")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun viewEventScreen_checkDeleteButton() {
+        // Set up the composable with a selected event
+        composeTestRule.setContent {
+            eventViewModel.selectedEvent = Event(1, "11-28-2023", "12:00", "14:00")
+            ViewEventScreen(eventViewModel, navController)
+        }
+
+        // Verify that the Delete button is displayed
+        composeTestRule
+            .onNodeWithText("Delete")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun viewEventScreen_checkBackButton() {
+        // Set up the composable with a selected event
+        composeTestRule.setContent {
+            eventViewModel.selectedEvent = Event(1, "11-28-2023", "12:00", "14:00")
+            ViewEventScreen(eventViewModel, navController)
+        }
+
+        // Verify that the Delete button is displayed
+        composeTestRule
+            .onNodeWithText("Done")
+            .assertIsDisplayed()
+    }
 }
