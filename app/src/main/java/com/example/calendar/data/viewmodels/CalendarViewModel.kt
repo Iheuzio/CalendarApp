9977857a3d7ccc0577304819711b10f5459bdcc0
Christopher <christopher.bartos@dawsoncollege.qc.ca>
2023-11-20 19:18:03 -0500

    Refactor CalendarViewModel
    Added documentation to the view model and removed unused methods

--- app/src/main/java/com/example/calendar/data/viewmodels/CalendarViewModel.kt
+++ app/src/main/java/com/example/calendar/data/viewmodels/CalendarViewModel.kt
@@ -6,16 +6,10 @@ import androidx.lifecycle.ViewModel
 import com.example.calendar.data.Event
 import java.util.*
 
-// Define a simple Event class for the calendar events
-
-
 class CalendarViewModel : ViewModel() {
 
     private val _selectedDate = mutableStateOf(Calendar.getInstance().time)
     val selectedDate: MutableState<Date> get() = _selectedDate
-
-    private val _isEventCreationDialogVisible = mutableStateOf(false)
-    val isEventCreationDialogVisible: MutableState<Boolean> get() = _isEventCreationDialogVisible
 
     private val _showDailyOverview = mutableStateOf(false)
     val showDailyOverview: MutableState<Boolean> get() = _showDailyOverview
@@ -23,6 +17,11 @@ class CalendarViewModel : ViewModel() {
     private val _events = mutableStateOf(listOf<Event>())
     val events: MutableState<List<Event>> get() = _events
 
+    /**
+     * Sets the selected date to the given date and toggles the visibility of the event creation dialog.
+     * If the given date is the same as the currently selected date, the visibility of the event creation dialog is toggled.
+     * Basically you need to press twice to go to the daily overview.
+     */
     fun onDateChange(newDate: Calendar) {
         if (_selectedDate.value == newDate.time) {
             _showDailyOverview.value = true
@@ -30,18 +29,9 @@ class CalendarViewModel : ViewModel() {
         _selectedDate.value = newDate.time
     }
 
-    fun onDialogDismiss() {
-        _isEventCreationDialogVisible.value = false
-    }
-
-    fun onEventSelected(event: Event) {
-    }
-
-    fun onAddEvent(event: Event) {
-        // Add the event to the list
-        _events.value = _events.value + event
-    }
-
+    /**
+     * Toggles the visibility of the event creation dialog.
+     */
     fun toggleShowDailyOverview() {
         _showDailyOverview.value = !_showDailyOverview.value
     }
