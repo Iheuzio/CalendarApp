9214ace1a3b6071ee8a625a868ba1e19a1fc8567
silalis <sila.ben-khelifa@dawsoncollege.qc.ca>
2023-11-20 20:04:28 -0500

    Begin tests for creating event screen

---
+++ app/src/androidTest/java/com/example/calendar/eventView/createEditEventTest.kt
@@ -0,0 +1,62 @@
+package com.example.calendar.eventView
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.calendar.data.Event
+import com.example.calendar.data.viewmodels.CalendarViewModel
+import com.example.calendar.data.viewmodels.EventViewModel
+import com.example.calendar.presentation.CalendarGrid
+import com.example.calendar.presentation.CreateEditEventScreen
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.Calendar
+
+@ExperimentalCoroutinesApi
+class CreateEditEventTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private lateinit var navController: TestNavHostController
+    private lateinit var eventViewModel : EventViewModel
+
+    @Before
+    fun setup() {
+        navController =
+            TestNavHostController(InstrumentationRegistry.getInstrumentation().targetContext)
+        eventViewModel = EventViewModel()
+        composeTestRule.setContent {
+            CreateEditEventScreen(viewModel = eventViewModel,
+                navController = navController,
+                inputDate = "11-28-2023",
+                inputEvent = Event(0, "11-28-2023", "12:00", "12:00", "Title")
+            )
+        }
+    }
+
+    @Test
+    fun calendarGrid_checkIfDaySelectedInit() {
+        // get current day and check if it is selected
+        //val initialDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        //Assert.assertEquals(initialDay, calendarModel.selectedDate.value.date)
+    }
+
+    @Test
+    fun createEditEventScreen_checkIfTitleIsDisplayed() {
+        composeTestRule
+            .onNodeWithText("Title")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun createEditEventScreen_checkIfDateIsDisplayed() {
+        composeTestRule
+            .onNodeWithText("Date: 11-28-2023")
+            .assertIsDisplayed()
+    }
+}
---
+++ app/src/androidTest/java/com/example/calendar/eventView/viewEventTest.kt
@@ -0,0 +1,4 @@
+package com.example.calendar.eventView
+
+class ViewEventTest {
+}
