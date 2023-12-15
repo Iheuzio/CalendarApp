ef2bd5e189d77f5bb6b52d8ae15a82f56786f32a
Christopher <christopher.bartos@dawsoncollege.qc.ca>
2023-12-15 13:57:51 -0500

    Add holiday data entity to database

---
+++ app/src/main/java/com/example/calendar/data/database/Holiday.kt
@@ -0,0 +1,22 @@
+package com.example.calendar.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Data class for the holiday entity
+ * @param id Int
+ * @param name String
+ * @param date String
+ * @param description String
+ * @param location List<String>
+ */
+@Entity
+data class Holiday(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    var name: String,
+    var date: String,
+    var description: String,
+    var location: List<String>
+)
