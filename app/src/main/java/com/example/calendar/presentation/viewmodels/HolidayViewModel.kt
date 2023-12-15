f4af1b075666838a1424c0d19741252239e58e94
Christopher <christopher.bartos@dawsoncollege.qc.ca>
2023-12-15 14:00:09 -0500

    ViewModel should use dao and data entity

--- app/src/main/java/com/example/calendar/presentation/viewmodels/HolidayViewModel.kt
+++ app/src/main/java/com/example/calendar/presentation/viewmodels/HolidayViewModel.kt
@@ -5,11 +5,18 @@ import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
-import com.example.calendar.data.GetHolidayData
+import com.example.calendar.data.database.Holiday
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import android.util.Log
+import com.example.calendar.data.database.AppDatabase
+import java.net.HttpURLConnection
+import java.net.URL
+import org.json.JSONArray
+import org.json.JSONTokener
 
-class HolidayViewModel: ViewModel() {
+class HolidayViewModel(private val database: AppDatabase) : ViewModel() {
     var holidays by mutableStateOf(listOf<Holiday>())
 
     init {
@@ -17,22 +24,62 @@ class HolidayViewModel: ViewModel() {
     }
 
     private fun getData() {
-        //Create a coroutine to fetch the data
-        viewModelScope.launch(Dispatchers.IO) {
-            val fetchedHolidays = GetHolidayData().fetchData()
+        viewModelScope.launch {
+            val getHolidayData = GetHolidayData()
+            val holidayList = getHolidayData.fetchData()
+            holidays = holidayList
 
-            // Update Compose state on the main thread
-            launch(Dispatchers.Main) {
-                holidays = fetchedHolidays
+            // Add holidays to database
+            for (holiday in holidayList) {
+                database.holidayDao().insertAll(holiday)
             }
         }
     }
 
-}
+    class GetHolidayData {
+        private val countryCode = java.util.Locale.getDefault().country
+        private val theUrl = "https://date.nager.at/api/v3/NextPublicHolidays/$countryCode"
 
-data class Holiday(
-    var date: String,
-    var name: String,
-    var description: String,
-    var location: List<String>,
-)
+        suspend fun fetchData(): List<Holiday> = withContext(Dispatchers.IO) {
+            val url = URL(theUrl)
+            val httpURLConnection = url.openConnection() as HttpURLConnection
+            httpURLConnection.requestMethod = "GET"
+            httpURLConnection.setRequestProperty("Accept", "text/json")
+
+            //Check if the connection is successful
+            val responseCode = httpURLConnection.responseCode
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val dataString = httpURLConnection.inputStream.bufferedReader()
+                    .use { it.readText() }
+
+                val holidays = mutableListOf<Holiday>()
+
+                val jsonArray = JSONTokener(dataString).nextValue() as JSONArray
+                for (i in 0 until jsonArray.length()) {
+                    // Reformat data
+                    val dateValues = jsonArray.getJSONObject(i).getString("date").split("-")
+                    val date = dateValues[1] + "-" + dateValues[2] + "-" + dateValues[0]
+
+                    val locations = jsonArray.getJSONObject(i).getString("counties").split(",")
+
+                    val holiday = Holiday(
+                        date = date,
+                        name = jsonArray.getJSONObject(i).getString("name"),
+                        description = jsonArray.getJSONObject(i).getString("localName"),
+                        location = locations
+                    )
+
+                    holidays += holiday
+
+                }
+
+                return@withContext holidays
+
+            } else {
+                Log.e("httpsURLConnection_ERROR", responseCode.toString())
+                return@withContext emptyList()
+            }
+        }
+    }
+}
